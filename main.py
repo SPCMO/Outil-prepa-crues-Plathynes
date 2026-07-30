4a007c8038be19547771597a98bc16f2203f72d4
PIOT Charles-Eddy <spcmo113466@gmail.com>
2026-07-30 19:36:10 +0200

    Import Plathynes : bouton ↺ appelle _refresh_visu_list + _plath_refresh ; doc aide.html mise à jour

--- main.py
+++ main.py
@@ -2705,7 +2705,8 @@ class App(tk.Tk):
         tk.Button(r, text="↺",
                   bg="#D6EAF8", fg="#1A5276", relief="groove", bd=1, padx=6, pady=2,
                   cursor="hand2",
-                  command=self._plath_refresh).pack(side=tk.LEFT)
+                  command=lambda: (self._refresh_visu_list(), self._plath_refresh())
+                  ).pack(side=tk.LEFT)
 
         # Ligne 2 : installation Plathynes
         r2 = self._row(inn_d, bg_d)
